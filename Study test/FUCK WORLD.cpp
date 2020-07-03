0c6c0478cc6d57cf1eb26b0b1fd14c7bbfc4cc67
Alan Xiong <AlanXiong2916886783@gmail.com>
2020-07-03 12:03:22 +0800

    1

--- Study test/FUCK WORLD.cpp
+++ Study test/FUCK WORLD.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
+#include"stdafx.h"   //包含预编译头文件
 
-int main() {
+#include<iostream>   //包含系统库
 
-	std::cout << "FUCK WORLD!" << std::endl;
+using namespace std;   //使用命名空间 std
+
+int   main   (int   argc,char*argv[])
+{
+	cout << "FUCK WORLD !" << endl;   //输出字符串信息
 
 	return 0;
 }
