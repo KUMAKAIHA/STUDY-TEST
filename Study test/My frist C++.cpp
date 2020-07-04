c026af7a7ca635933d364b0e59fdd17cdaad4f45
Alan Xiong <AlanXiong2916886783@gmail.com>
2020-07-04 13:51:51 +0800

    示例1

---
+++ Study test/My frist C++.cpp
@@ -0,0 +1,11 @@
+//书中的示例
+
+#include <iostream>      //预处理器指令
+int main()               //函数头
+{                        //函数体开始
+	using namespace std; //使用命名空间
+	cout << "Come up and C++ me some time.";  //信息
+	cout << endl;                             //开始新的一行
+	cout << "You won't regret it!" << endl;   //更多的输出
+	return 0;                                 //终止函数
+}                                             //函数体结束
